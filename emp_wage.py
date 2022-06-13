33b2602d9e9829491f64926697df4fd8705f976f
chakravarthy <m.chakra473@gmail.com>
2022-06-13 08:56:32 +0530

    first commit

---
+++ emp_wage.py
@@ -0,0 +1,9 @@
+import random
+import sys
+if __name__ == "__main__":
+    n = len(sys.argv)
+    print(n)
+    res = int(sys.argv[1])*int(sys.argv[1])
+    com = int(sys.argv[2])+int(sys.argv[2])
+    print(res)
+    print(com)
