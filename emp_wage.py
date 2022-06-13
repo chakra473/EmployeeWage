e18db6457edc598128a3c6c22a72e41b1fc6c106
chakravarthy <m.chakra473@gmail.com>
2022-06-13 15:57:09 +0530

    [chakravarthy]Add:Daily wage calculation

--- emp_wage.py
+++ emp_wage.py
@@ -31,5 +31,26 @@ def  employee_attendence():
         return "EMPLOYEE IS ABSENT"
 
 
+def daily_wage():
+    """
+    Description:
+        This method calculates daily wage of the employee
+    Parameter:
+        None
+    Return:
+        Returns salary in number(int)
+    """
+    attendence=random.randrange(0,2)
+    if attendence == PRESENT:
+        emp_rate_per_hr = 20
+        emp_hrs = 8
+        salary = emp_rate_per_hr * emp_hrs
+        return salary
+    else:
+        salary = 0
+        return salary
+
+
 if __name__ =="__main__":
     print(employee_attendence())
+    print(daily_wage())
