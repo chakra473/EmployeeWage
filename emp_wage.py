0760a0d8a25a6d2d0a37a82f34e416cd1a2bb55c
chakra473 <91408040+chakra473@users.noreply.github.com>
2022-06-13 08:57:30 +0530

    "first commit"

--- emp_wage.py
+++ emp_wage.py
@@ -1,9 +1,1 @@
-import random
-import sys
-if __name__ == "__main__":
-    n = len(sys.argv)
-    print(n)
-    res = int(sys.argv[1])*int(sys.argv[1])
-    com = int(sys.argv[2])+int(sys.argv[2])
-    print(res)
-    print(com)
+
