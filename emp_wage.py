1de316a3f87066254c897530f8caa1e1feda5d96
chakravarthy <m.chakra473@gmail.com>
2022-06-13 09:34:58 +0530

    [chakravarthy]Add:Check Employee is Present or Absent

--- emp_wage.py
+++ emp_wage.py
@@ -1,1 +1,34 @@
 #WELCOME TO EMPLOYEE WAGE PROBLEM
+'''
+@Author: Chakravarthy
+
+@Date: 2022-06-13 9:00:00
+
+@Last Modified by: Chakravarthy
+
+@Last Modified time: 2022-06-13 9:00:00
+
+@Title : EMPLOYEE WAGE
+'''
+import random
+
+
+PRESENT = 1
+ABSENT = 0
+def uc1():
+    """
+    Description:
+        This method checks whether employee is present or absent
+    Parameter:
+        None
+    Return:
+        Returns string
+    """
+    attendence =random.randrange(0,2)
+    if attendence == PRESENT:
+        return "EMPLOYEE IS PRESENT"
+    else:
+        return "EMPLOYEE IS ABSENT"
+print(uc1())
+
+    
