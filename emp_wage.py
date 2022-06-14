64eaf58b80f7c1daa5b28aaf9306d8cf05778111
chakravarthy <m.chakra473@gmail.com>
2022-06-14 09:42:36 +0530

    [chakravarthy]Refactor: Calculation of Part time Employee Wage

--- emp_wage.py
+++ emp_wage.py
@@ -13,75 +13,21 @@
 import random
 
 
-PRESENT = 1
-ABSENT = 0
-PART_TIME = 2
-def  employee_attendence():
-    """
-    Description:
-        This method checks whether employee is present or absent
-    Parameter:
-        None
-    Return:
-        Returns string
-    """
-    attendence =random.randrange(0,2)
-    if attendence == PRESENT:
-        return "EMPLOYEE IS PRESENT"
-    else:
-        return "EMPLOYEE IS ABSENT"
-
-
-def daily_wage():
-    """
-    Description:
-        This method calculates daily wage of the employee
-    Parameter:
-        None
-    Return:
-        Returns salary in number(int)
-    """
-    attendence=random.randrange(0,2)
-    if attendence == PRESENT:
-        emp_rate_per_hr = 20
-        emp_hrs = 8
-        salary = emp_rate_per_hr * emp_hrs
-        return salary
-    else:
-        salary = 0
-        return salary
-
-
-def part_time_wage():
-    """
-    Description:
-        This method calculates daily wage of the employee both full time and part time 
-    Parameter:
-        None
-    Return:
-        Returns salary in number(int)
-    """
-    attendence=random.randrange(0,3)
-    if attendence == PRESENT:
-        emp_rate_per_hr = 20
-        emp_hrs = 8
-        salary = emp_rate_per_hr * emp_hrs
+EMP_RATE_PER_HR = 20
+attendence =random.randint(0,1)
+if attendence == 1:
+    print("EMPLOYEE IS PRESENT")
+    time_check = random.randint(1,2)
+    if time_check == 2:
+        salary = EMP_RATE_PER_HR * 8 # working hours of a employee in a day(full time) is 8
         print("EMPLOYEE IS PRESENT FULL TIME")
-        return salary
-    elif attendence == PART_TIME:
-        emp_rate_per_hr = 20
-        emp_hrs = 4
-        salary = emp_rate_per_hr * emp_hrs
-        print("EMPLOYEE IS PRESENT PART TIME")
-        return salary
-
+        print(f"SALARY OF AN EMPLOYEE IS {salary}")
     else:
-        salary = 0
-        print("EMPLOYEE IS ABSENT")
-        return salary
+        salary = EMP_RATE_PER_HR * 4 # working hours of a employee in a day(part time) is 4
+        print("EMPLOYEE IS PRESENT PART TIME")
+        print(f"SALARY OF AN EMPLOYEE IS {salary}")
 
-
-if __name__ =="__main__":
-    print(employee_attendence())
-    print(daily_wage())
-    print(part_time_wage())
+else:
+    print("EMPLOYEE IS ABSENT")
+    salary = 0
+    print(f"SALARY OF AN EMPLOYEE IS {salary}")
