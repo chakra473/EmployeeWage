61e68136ea0189105a55b939f4f5ad21914099f6
chakravarthy <m.chakra473@gmail.com>
2022-06-14 09:35:30 +0530

    [chakravarthy]Refactor: Calculation of Daily Employee Wage

--- emp_wage.py
+++ emp_wage.py
@@ -13,44 +13,13 @@
 import random
 
 
-PRESENT = 1
-ABSENT = 0
-def  employee_attendence():
-    """
-    Description:
-        This method checks whether employee is present or absent
-    Parameter:
-        None
-    Return:
-        Returns string
-    """
-    attendence =random.randrange(0,2)
-    if attendence == PRESENT:
-        return "EMPLOYEE IS PRESENT"
-    else:
-        return "EMPLOYEE IS ABSENT"
-
-
-def daily_wage():
-    """
-    Description:
-        This method calculates daily wage of the employee
-    Parameter:
-        None
-    Return:
-        Returns salary in number(int)
-    """
-    attendence=random.randrange(0,2)
-    if attendence == PRESENT:
-        emp_rate_per_hr = 20
-        emp_hrs = 8
-        salary = emp_rate_per_hr * emp_hrs
-        return salary
-    else:
-        salary = 0
-        return salary
-
-
-if __name__ =="__main__":
-    print(employee_attendence())
-    print(daily_wage())
+EMP_RATE_PER_HR = 20
+attendence =random.randint(0,1)
+if attendence == 1:
+    print("EMPLOYEE IS PRESENT")
+    salary = EMP_RATE_PER_HR * 8 # working hours of a employee in a day is 8
+    print(f"SALARY OF AN EMPLOYEE IS {salary}")
+else:
+     print("EMPLOYEE IS ABSENT")
+     salary = 0
+     print(f"SALARY OF AN EMPLOYEE IS {salary}")
