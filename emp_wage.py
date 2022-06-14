ed26ff764668aa647e6d08597e4dbd6f2a738cfe
chakravarthy <m.chakra473@gmail.com>
2022-06-14 10:25:29 +0530

    [chakravarthy]Refactor: Calculation of Part time Employee Wage

--- emp_wage.py
+++ emp_wage.py
@@ -1,13 +1,8 @@
 #WELCOME TO EMPLOYEE WAGE PROBLEM
-'''
-@Author: Chakravarthy
-
+'''@Author: Chakravarthy
 @Date: 2022-06-13 9:00:00
-
 @Last Modified by: Chakravarthy
-
 @Last Modified time: 2022-06-13 9:00:00
-
 @Title : EMPLOYEE WAGE
 '''
 import random
@@ -20,14 +15,10 @@ if attendence == 1:
     time_check = random.randint(1,2)
     if time_check == 2:
         salary = EMP_RATE_PER_HR * 8 # working hours of a employee in a day(full time) is 8
-        print("EMPLOYEE IS PRESENT FULL TIME")
-        print(f"SALARY OF AN EMPLOYEE IS {salary}")
+        print(f"EMPLOYEE IS PRESENT FULL TIME\nSALARY OF AN EMPLOYEE IS {salary}")
     else:
         salary = EMP_RATE_PER_HR * 4 # working hours of a employee in a day(part time) is 4
-        print("EMPLOYEE IS PRESENT PART TIME")
-        print(f"SALARY OF AN EMPLOYEE IS {salary}")
+        print(f"EMPLOYEE IS PRESENT PART TIME\nSALARY OF AN EMPLOYEE IS {salary}")
 
 else:
     print("EMPLOYEE IS ABSENT")
-    salary = 0
-    print(f"SALARY OF AN EMPLOYEE IS {salary}")
